0d39b4b8f45456a32c704b7534db870334d9c18c
Antoine Ghinciulesco-Saulgeot <antoine.ghinciulesco-saulgeot@ens.uvsq.fr>
2018-11-29 18:52:39 +0100

    try texturing

--- IN505/Rock.cc
+++ IN505/Rock.cc
@@ -4,6 +4,10 @@
 #include "Objects.h"
 #include "mes_formes.h"
 #include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 using namespace std;
 
@@ -67,14 +71,71 @@ void Rock::print()
 }
 void Rock::print_Rectangle()
 {
-		Rectangle *rect= dynamic_cast <Rectangle*> (this->getForme());
+/*
+unsigned int texture;
+glGenTextures(1, &texture);
+glBindTexture(GL_TEXTURE_2D, texture);
+// set the texture wrapping/filtering options (on the currently bound texture object)
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+// load and generate the texture
+int width, height, nrChannels;
+unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+if (data)
+{
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+}
+else
+{
+    std::cout << "Failed to load texture" << std::endl;
+}
+stbi_image_free(data);*/
+/*int width, height, nrChannels;
+
+width =830;
+height = 710;
+
+   glEnable(GL_TEXTURE_2D);
+   GLuint texture;
+   texture = loadTexture("MAVERICK", width, height);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+        glGenTextures(1, &texture);
+       glBindTexture(GL_TEXTURE_2D, texture);
+//        stbi_image_free(texture);
+
+
+ // draw stuff here
+
+        // render container
+
+
+     glLoadIdentity;
+   glTranslatef(-5, 0, -15);
+   glBindTexture(GL_TEXTURE_2D, texture);
+*/
+    Rectangle *rect= dynamic_cast <Rectangle*> (this->getForme());
     glBegin(GL_QUADS);
         glColor3f(0.5, 0.5, 0.5);
+//        glBindTexture(GL_TEXTURE_2D, texture);
+                glTexCoord2f(1, 0);
+
         glVertex2i(rect->getP1().getX(),rect->getP1().getY());
+               glTexCoord2f(0, 0);
+
         glVertex2i(rect->getP2().getX(),rect->getP2().getY());
+               glTexCoord2f(0, 1);
+
         glVertex2i(rect->getP3().getX(),rect->getP3().getY());
+               glTexCoord2f(1, 1);
+
         glVertex2i(rect->getP4().getX(),rect->getP4().getY());
     glEnd();
+    glDisable(GL_TEXTURE_2D);
+
 }
 
 void Rock::print_Triangle()
@@ -83,11 +144,12 @@ void Rock::print_Triangle()
 
     glBegin(GL_TRIANGLES);
         glColor3f(0.5, 0.5, 0.5);
-        glVertex2i(tria->getP1().getX(),tria->getP1().getY());
-        glVertex2i(tria->getP2().getX(),tria->getP2().getY());
-        glVertex2i(tria->getP3().getX(),tria->getP3().getY());
+        //glVertex2i(tria->getP1().getX(),tria->getP1().getY());
+        //glVertex2i(tria->getP2().getX(),tria->getP2().getY());
+        //glVertex2i(tria->getP3().getX(),tria->getP3().getY());
 
     glEnd();
 }
 Rock::~Rock()
 {}
+
